6b1ea4aee154ac5da3f386dfefaa70044140ded3
Dricovw <wijk0032@hz.nl>
2025-04-12 18:00:42 +0200

    changed the source file to the src folder

--- AirUFV/src/Program.cs
+++ AirUFV/src/Program.cs
@@ -9,10 +9,15 @@ internal class Program
 {
 
     public static void Main() {
-        Console.writeLine("----------   Air UFV ------------");
-        Console.writeLine();
-        Console.writeLine();
-        Console.writeLine();
+        Console.WriteLine("----------   Air UFV  ------------");
+        Console.WriteLine("1. Load flight from file.");
+        Console.WriteLine("2. Load a flight manually.");
+        Console.WriteLine("3. Start simulation (Manual).");
+        Console.WriteLine("4. Start simulation (Automatic).");
+        Console.WriteLine("5. Exit.");
+        Console.WriteLine("----------------------------------");
+
+        // int input = int32.Parse(Console.ReadLine());
     }
 
 }
