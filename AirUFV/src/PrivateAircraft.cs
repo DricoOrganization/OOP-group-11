0d20548e673cb4c6234d25a52bdca14f35ba26b4
Hugo Losada <129153062+Yodugo@users.noreply.github.com>
2025-04-14 14:47:14 +0200

    Create PrivateAircraft.cs

---
+++ AirUFV/src/PrivateAircraft.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PRACTICALWORKI
+{
+    public class PrivateAircraft : Aircraft
+    {
+        public string owner { get; set; }
+
+        public PrivateAircraft(string id, AircraftStatus status, int distance, int speed, double fuelCapacity, double fuelConsumption, double currentFuel, string owner)
+            : base(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel)
+        {
+            this.owner = owner;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Type: Private | Owner: {this.owner}";
+        }
+    }
+}
