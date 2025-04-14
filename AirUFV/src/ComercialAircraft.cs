74669ad0141ce4740b96fff1966e68c2b55130d7
Hugo Losada <129153062+Yodugo@users.noreply.github.com>
2025-04-14 14:46:48 +0200

    Create ComercialAircraft.cs

---
+++ AirUFV/src/ComercialAircraft.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PRACTICALWORKI
+{
+     public class CommercialAircraft : Aircraft
+    {
+        public int numberOfPassengers { get; set; }
+
+        public CommercialAircraft(string id, AircraftStatus status, int distance, int speed, double fuelCapacity, double fuelConsumption, double currentFuel, int numberOfPassengers)
+            : base(id, status, distance, speed, fuelCapacity, fuelConsumption, currentFuel)
+        {
+            this.numberOfPassengers = numberOfPassengers;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Type: Commercial | Passengers: {this.numberOfPassengers}";
+        }
+    }
+}
